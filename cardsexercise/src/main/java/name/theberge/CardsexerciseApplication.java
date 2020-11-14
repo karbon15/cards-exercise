a6810a1ef8434f178e605c49e2905786157d6d6d
Pier-Luc Theberge <pier-luc@theberge.name>
2020-11-14 09:22:36 -0500

    Adds the initial scaffolding

---
+++ cardsexercise/src/main/java/name/theberge/CardsexerciseApplication.java
@@ -0,0 +1,13 @@
+package name.theberge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsexerciseApplication.class, args);
+	}
+
+}
