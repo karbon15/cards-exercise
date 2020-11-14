fe38583a4741085f79b24a311b930721c0c94238
Pier-Luc Theberge <pier-luc@theberge.name>
2020-11-14 09:22:36 -0500

    Adds the initial scaffolding

---
+++ cardsexercise/src/test/java/name/theberge/CardsexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package name.theberge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
